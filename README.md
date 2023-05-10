1277813ba4888666e0b43d4d756a0f6b1b6534e4
007ebey <007ebey@gmail.com>
2023-05-10 13:06:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Slider-Snapshot-Learning
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-gs2qx5)
---
+++ index.js
@@ -0,0 +1,83 @@
+// Import stylesheets
+import './style.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import React from 'react';
+
+class ImageSlider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImage: 0,
+      images: [
+        'https://picsum.photos/400/200?random=1',
+        'https://picsum.photos/500/200?random=2',
+        'https://picsum.photos/400/300?random=3',
+        'https://picsum.photos/600/200?random=4',
+        'https://picsum.photos/200/200?random=5',
+      ],
+      isSliding: false,
+      transitionDuration: 1000,
+    };
+    this.sliderRef = React.createRef();
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.slideImage();
+    }, 10000);
+  }
+
+  slideImage = () => {
+    const { currentImage, images } = this.state;
+    const nextImage = (currentImage + 1) % images.length;
+    this.setState({ currentImage: nextImage, isSliding: true });
+    setTimeout(() => {
+      this.setState({ isSliding: false });
+    }, this.state.transitionDuration);
+  };
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    const { isSliding } = this.state;
+    if (isSliding) {
+      const node = this.sliderRef.current;
+      const currentImage = this.state.currentImage;
+      const currentSlide = node.getElementsByTagName('img')[0];
+      console.log(currentSlide);
+      const { width, height } = currentSlide;
+      console.log(width + height);
+      return { width, height };
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (snapshot !== null) {
+      const node = this.sliderRef.current;
+      const currentImage = this.state.currentImage;
+      const currentSlide = node.getElementsByTagName('img')[0];
+      const { width: prevWidth, height: prevHeight } = snapshot;
+      const { width: currentWidth, height: currentHeight } =
+        currentSlide.getBoundingClientRect();
+      if (prevWidth !== currentWidth || prevHeight !== currentHeight) {
+        node.style.width = currentWidth + 'px';
+        node.style.height = currentHeight + 'px';
+      }
+    }
+  }
+
+  render() {
+    const { images, currentImage } = this.state;
+    return (
+      <div ref={this.sliderRef} className="slider">
+        <img src={images[currentImage]} alt={`Current Image`} />
+      </div>
+    );
+  }
+}
+
+export default ImageSlider;
+
+ReactDOM.render(<ImageSlider />, document.getElementById('app'));
